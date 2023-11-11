7dff9d81c0f89fa6438fe2daee6d2f1af34da079
Dwesh163 Desktop <ehouarn.duriaux@protonmail.com>
2023-11-11 17:44:37 +0100

    [add] Telegram Bot

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 from getpass import getpass
 from python_aternos import Client, atserver
 from dotenv import load_dotenv
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 import os
 
 atclient = Client()
 aternos = atclient.account
 atclient.login_with_session(os.getenv("ATERNOS_SESSION_COOKIE"))
+srvs = aternos.list_servers()
+
+TOKEN = os.getenv('BOT_ATERNOS_TOKEN')
+
+def main():
+
+    updater = Updater(TOKEN, use_context=True)
+
+    dp = updater.dispatcher
+
+    # Run bot
+    updater.start_polling()
+
+    # Stop bot with CTRL+C
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
